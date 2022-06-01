918559dab361c2f2a55545aff1f578c860707c1c
Lina Zoo <linazooper@gmail.com>
2022-06-01 14:54:10 -0500

    abstracted all of the different actions into their own action creator so when you need an action you call that specific action creatorand pass on any data that you want to add to the specific action

--- index.js
+++ index.js
@@ -1,75 +1,4 @@
-{
-  type: "ADD_TODO";
-  todo: {
-    id: 0;
-    name: "learn Redux";
-    complete: false;
-  }
-}
-{
-  type: "REMOVE_TODO";
-  id: 0;
-}
-
-{
-  type: "TOGGLE_TODO";
-  id: 0;
-}
-
-{
-  type: "ADD_GOAL";
-  goal: {
-    id: 0;
-    name: "Run a marathon";
-  }
-}
-
-{
-  type: "REMOVE_GOAL";
-  id: 0;
-}
-
-//pure function
-//1 always return the same result if the exact same arguments are passed in
-//2 depend only on the arguments passed to them and don't acess values outside of their scope
-//3 should never produce any side effects, no ajax request, no DOM interactions etc
-
-// Reducer function
-function todos(state = [], action) {
-  switch (action.type) {
-    case "ADD_TODO":
-      return state.concat([action.todo]);
-    case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TODO":
-      return state.map((todo) =>
-        todo.id !== action.id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-    default:
-      return state;
-  }
-}
-
-function goals(state = [], action) {
-  switch (action.type) {
-    case "ADD_GOAL":
-      return state.concat([action.goal]);
-    case "REMOVE_GOAL":
-      return state.filter((goal) => goal.id !== action.id);
-    default:
-      return state;
-  }
-}
-
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  };
-}
-
+//Library Code
 function createStore(reducer) {
   //the store should have four parts
 
@@ -103,66 +32,133 @@ function createStore(reducer) {
   };
 }
 
+// App Code
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+function toggleTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+//pure function
+//1 always return the same result if the exact same arguments are passed in
+//2 depend only on the arguments passed to them and don't acess values outside of their scope
+//3 should never produce any side effects, no ajax request, no DOM interactions etc
+
+// Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
 const store = createStore(app);
 
 store.subscribe(() => {
   console.log("The new state is: ", store.getState());
 });
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+//a
+store.dispatch(
+  addTodoAction({
     id: 0,
     name: "Walk the dog",
     complete: false,
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 1,
     name: "Wash the car",
     complete: false,
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+store.dispatch(
+  addTodoAction({
     id: 2,
     name: "Go to the gym",
     complete: true,
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "REMOVE_TODO",
-  id: 1,
-});
+store.dispatch(removeTodoAction(1));
 
-store.dispatch({
-  type: "TOGGLE_TODO",
-  id: 0,
-});
+store.dispatch(toggleTodoAction(0));
 
-store.dispatch({
-  type: "ADD_GOAL",
-  goal: {
+store.dispatch(
+  addGoalAction({
     id: 0,
     name: "Learn Redux",
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "ADD_GOAL",
-  goal: {
+store.dispatch(
+  addGoalAction({
     id: 1,
     name: "Lose 20 pounds",
-  },
-});
+  })
+);
 
-store.dispatch({
-  type: "REMOVE_GOAL",
-  id: 0,
-});
+store.dispatch(removeGoalAction(0));
